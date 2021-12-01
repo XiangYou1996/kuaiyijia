f26e92569c32842f2080627d46d3dc08d7066605
yunfanfan <pengyunfan@icloud.com>
2021-12-01 14:33:29 +0800

    2021.12.1

--- app/src/main/java/com/example/kuaiyijia/UI/ActivityIndex.java
+++ app/src/main/java/com/example/kuaiyijia/UI/ActivityIndex.java
@@ -6,7 +6,9 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -14,20 +16,29 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.kuaiyijia.Adapter.VpAdapter;
 import com.example.kuaiyijia.R;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.bean.ZxingConfig;
+import com.yzq.zxinglibrary.common.Constant;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ActivityIndex extends AppCompatActivity {
 
     private static final String TAG = "banner：";
     private int currentPosition;
+    private static final int REQUEST_CODE_SCAN = 1;
+    private String mResultScan = null;
     private Button mBtnSecond;
+
+    private EditText mYundanId;
     private ImageButton mIBtnSearch;
     private ImageButton mIBtnScan;
     private Button mBtnIndexNewOrder;
@@ -41,9 +52,11 @@ public class ActivityIndex extends AppCompatActivity {
     private Button mBtnDialogNetPoint3;
     private Button mBtnDialogNetPoint4;
 
+
     private void initView() {
         mBtnSecond = findViewById(R.id.bt_second);
         mIBtnSearch = findViewById(R.id.bt_search);
+        mYundanId = findViewById(R.id.et_search);
         mIBtnScan = findViewById(R.id.bt_scan);
         mBtnIndexNewOrder = findViewById(R.id.bt_index_newOrder);
         mBtnIndexSendCar = findViewById(R.id.bt_index_sendCar);
@@ -138,44 +151,20 @@ public class ActivityIndex extends AppCompatActivity {
                         break;
                 }
             }
-            private void setDialog() {
-                Dialog mChangNetPointDialog = new Dialog(ActivityIndex.this, R.style.BottomDialog);
-                mChangNetPointDialog.setTitle("更换当前网点");
-                LinearLayout root = (LinearLayout) LayoutInflater.from(ActivityIndex.this).inflate(
-                        R.layout.bottom_dialog, null);
-                //初始化视图
-                root.findViewById(R.id.btn_dialog_netpoint1).setOnClickListener(this);
-                root.findViewById(R.id.btn_dialog_netpoint2).setOnClickListener(this);
-                root.findViewById(R.id.btn_dialog_netpoint3).setOnClickListener(this);
-                root.findViewById(R.id.btn_dialog_netpoint4).setOnClickListener(this);
-                mChangNetPointDialog.setContentView(root);
-                Window dialogWindow = mChangNetPointDialog.getWindow();
-                dialogWindow.setGravity(Gravity.BOTTOM);
-//        dialogWindow.setWindowAnimations(R.style.dialogstyle); // 添加动画
-                WindowManager.LayoutParams lp = dialogWindow.getAttributes(); // 获取对话框当前的参数值
-                lp.x = 0; // 新位置X坐标
-                lp.y = 0; // 新位置Y坐标
-                lp.width = (int) getResources().getDisplayMetrics().widthPixels; // 宽度
-                root.measure(0, 0);
-                lp.height = root.getMeasuredHeight();
 
-                lp.alpha = 9f; // 透明度
-                dialogWindow.setAttributes(lp);
-                mChangNetPointDialog.show();
-            }
         });
         //2.搜索
         mIBtnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                handlerSearch(v);
             }
         });
         //3.扫码
         mIBtnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                scan(v);
             }
         });
         //4.fun1 新运单
@@ -214,5 +203,67 @@ public class ActivityIndex extends AppCompatActivity {
             }
         });
 
+
+    }
+    private void setDialog() {
+        Dialog mChangNetPointDialog = new Dialog(ActivityIndex.this, R.style.BottomDialog);
+        mChangNetPointDialog.setTitle("更换当前网点");
+        LinearLayout root = (LinearLayout) LayoutInflater.from(ActivityIndex.this).inflate(
+                R.layout.bottom_dialog, null);
+        //初始化视图
+        root.findViewById(R.id.btn_dialog_netpoint1).setOnClickListener(this);
+        root.findViewById(R.id.btn_dialog_netpoint2).setOnClickListener(this);
+        root.findViewById(R.id.btn_dialog_netpoint3).setOnClickListener(this);
+        root.findViewById(R.id.btn_dialog_netpoint4).setOnClickListener(this);
+        mChangNetPointDialog.setContentView(root);
+        Window dialogWindow = mChangNetPointDialog.getWindow();
+        dialogWindow.setGravity(Gravity.BOTTOM);
+//        dialogWindow.setWindowAnimations(R.style.dialogstyle); // 添加动画
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes(); // 获取对话框当前的参数值
+        lp.x = 0; // 新位置X坐标
+        lp.y = 0; // 新位置Y坐标
+        lp.width = (int) getResources().getDisplayMetrics().widthPixels; // 宽度
+        root.measure(0, 0);
+        lp.height = root.getMeasuredHeight();
+
+        lp.alpha = 9f; // 透明度
+        dialogWindow.setAttributes(lp);
+        mChangNetPointDialog.show();
+    }
+
+    private void handlerSearch(View v) {
+        String yundanId = mYundanId.getText().toString();
+        if (TextUtils.isEmpty(yundanId)) {
+            Toast.makeText(this, "运单号不可以为空..", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+    }
+    public void scan(View view) {
+        Intent intent = new Intent(new Intent(this, CaptureActivity.class));
+
+        ZxingConfig config = new ZxingConfig();
+        config.setShowAlbum(true);
+        config.setShowFlashLight(true);
+        intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+
+        startActivityForResult(intent, REQUEST_CODE_SCAN);
+        if (mResultScan != null) {
+            Log.d(TAG, "scan: " + mResultScan);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+
+                mResultScan = data.getStringExtra(Constant.CODED_CONTENT);
+                Log.d(TAG, "onActivityResult: " + mResultScan);
+
+            }
+        }
     }
 }
